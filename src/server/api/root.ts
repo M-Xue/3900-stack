02fe6fb3c6ea8780a338c5e3bfc47a9e7591fbae
Max Xue <maxxue1512@gmail.com>
2023-06-10 02:40:15 +1000

    Added server error example

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,10 +1,14 @@
 import { itemRouter } from './routes/items';
 import { z } from 'zod';
 import { publicProcedure, router } from './trpc';
+import { TRPCError } from '@trpc/server';
 
 export const appRouter = router({
 	getHello: publicProcedure.query(({ ctx }) => {
 		return { hello: 'world' };
+	}),
+	getError: publicProcedure.query(() => {
+		throw new TRPCError({ message: 'Test Error', code: 'BAD_REQUEST' });
 	}),
 	items: itemRouter,
 });
