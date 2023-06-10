fd699d291d52431aac803aa983e59b44855a3cef
Max Xue <maxxue1512@gmail.com>
2023-06-10 15:16:36 +1000

    Added error handling on main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,14 +6,18 @@ import Form from './Form';
 import { trpc } from '@/utils/trpc';
 
 export default function Home() {
-	const { data, isLoading } = trpc.items.getItems.useQuery();
-
+	const { data, isLoading, isError, error } = trpc.items.getItems.useQuery();
 	return (
 		<div className={styles['main']}>
 			<div className={styles['root-container']}>
 				<Form />
 				{isLoading ? (
 					<div>Loading...</div>
+				) : isError ? (
+					<div>
+						<div>Error</div>
+						<div>{error.message}</div>
+					</div>
 				) : (
 					<div>
 						{data?.map((item) => (
